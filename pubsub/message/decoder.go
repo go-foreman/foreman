1e4199cd16e344201feedac0594cf5e44765c8d5
kopaygorodsky <vlad.kopaygorodsky@gmail.com>
2021-04-16 17:58:11 +0300

    fix

--- pubsub/message/decoder.go
+++ pubsub/message/decoder.go
@@ -171,7 +171,7 @@ func(j jsonDecoder) setGroupKind(obj Object) error {
 		obj.SetGroupKind(gk)
 	}
 
-	//we do not allow nested Object
+	//we not allow nested Object
 	structVal := reflect.ValueOf(obj)
 	structType := structVal.Type().Elem()
 
