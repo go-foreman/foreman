206a77a10f706eb2db11040e5dda5c467628b056
kopaygorodsky <vlad.kopaygorodsky@gmail.com>
2021-04-18 02:55:25 +0300

    added test for saga uid svc

---
+++ saga/sagaid_test.go
@@ -0,0 +1,38 @@
+package saga
+
+import (
+	"fmt"
+	"github.com/go-foreman/foreman/pubsub/message"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNewSagaUIDService(t *testing.T) {
+	svc := NewSagaUIDService()
+
+	t.Run("add and fetch saga uid", func(t *testing.T) {
+		headers := message.Headers{}
+		svc.AddSagaId(headers, "xxx")
+		extractedUID, err := svc.ExtractSagaUID(headers)
+		require.NoError(t, err)
+		assert.Equal(t, "xxx", extractedUID)
+	})
+
+	t.Run("saga uid not found", func(t *testing.T) {
+		headers := message.Headers{}
+		extractedUID, err := svc.ExtractSagaUID(headers)
+		assert.Error(t, err)
+		assert.EqualError(t, err, fmt.Sprintf("saga uid was not found in headers by key %s", sagaUIDKey))
+		assert.Empty(t, extractedUID)
+	})
+
+	t.Run("saga uid is not string type in headers", func(t *testing.T) {
+		headers := message.Headers{}
+		headers[sagaUIDKey] = 1
+		extractedUID, err := svc.ExtractSagaUID(headers)
+		assert.Error(t, err)
+		assert.EqualError(t, err, "Saga uid was found, but has wrong type, should be string")
+		assert.Empty(t, extractedUID)
+	})
+}
