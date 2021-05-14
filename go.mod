e388c66dc2fa4cbc6a80f368403e0031f9b0a0b2
kopaygorodsky <vlad.kopaygorodsky@gmail.com>
2021-05-15 00:36:37 +0300

    added support for postgres saga store + integration test

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/google/uuid v1.1.2
+	github.com/jackc/pgx/v4 v4.11.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.1
 	github.com/pkg/errors v0.9.1
 	github.com/streadway/amqp v1.0.0
---
+++ testing/integration/saga/pgstore_test.go
@@ -0,0 +1,36 @@
+package saga
+
+import (
+	"github.com/go-foreman/foreman/pubsub/message"
+	"github.com/go-foreman/foreman/runtime/scheme"
+	"github.com/go-foreman/foreman/saga"
+	intSuite "github.com/go-foreman/foreman/testing/integration/saga/suite"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type pgStoreTest struct {
+	intSuite.PgSuite
+}
+
+func TestPGSuite(t *testing.T) {
+	pgStoreTest := &pgStoreTest{}
+	suite.Run(t, pgStoreTest)
+}
+
+func (p *pgStoreTest) TestPGStore() {
+	t := p.T()
+
+	dbConnection := p.Connection()
+	schemeRegistry := scheme.NewKnownTypesRegistry()
+	schemeRegistry.AddKnownTypes(testGroup, &WorkflowSaga{})
+	schemeRegistry.AddKnownTypes(testGroup, &FilterSaga{})
+	marshaller := message.NewJsonMarshaller(schemeRegistry)
+	pgStore, err := saga.NewSQLSagaStore(dbConnection, saga.PGDriver, marshaller)
+
+	require.NoError(t, err)
+	require.NotNil(t, pgStore)
+
+	testUseCases(t, pgStore, schemeRegistry, dbConnection)
+}
