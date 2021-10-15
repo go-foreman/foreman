c58d3195b22538d030b5a2cff7e360fcbb19680a
Vladyslav Kopaihorodskyi <vlad.kopaygorodsky@gmail.com>
2021-10-16 02:01:10 +0300

    Added comments

--- pubsub/message/execution/executor.go
+++ pubsub/message/execution/executor.go
@@ -1,3 +1,5 @@
 package execution
 
+// Executor is a callback that will be called on received message with context.
+// it should return an error only if internal server error happened, in case of business errors it's best to send a failure event.
 type Executor func(execCtx MessageExecutionCtx) error
